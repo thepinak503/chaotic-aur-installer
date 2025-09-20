7edead9af9406d3c0bc7fb423ea3aadb66baa73e
Pinak Dhabu <thepinak503@duck.com>
2025-09-20 23:08:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm \
-  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+  'https://mirror.ufscar.br/chaotic-aur/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
   'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 if ! grep -q '\[chaotic-aur\]' /etc/pacman.conf; then
   echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
